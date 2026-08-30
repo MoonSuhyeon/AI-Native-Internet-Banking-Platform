a597f2915e78d0d40c4d4d6e8590e033bc84b0f0
문수현 <dalmate777@gmail.com>
2026-08-30 21:35:58 +0900

    히어로 이미지 크기 및 위치 조정

--- web/components/home/HeroCarousel.tsx
+++ web/components/home/HeroCarousel.tsx
@@ -169,12 +169,12 @@ export default function HeroCarousel({ current, paused, onChangeTo, onPausedChan
           src={`/images/personal-hero${current + 1}.png`}
           alt={slide.badge}
           width={
-            current === 0 ? 426 :
+            current === 0 ? 449 :
             current === 1 ? 634 :
             621
           }
           height={
-            current === 0 ? 269 :
+            current === 0 ? 284 :
             current === 1 ? 402 :
             395
           }
